19259066cda2d89c4266e1bde92c7b7382087d94
TheDudeFromCI <thedudefromci@gmail.com>
2022-01-29 17:05:50 -0700

    Removed resourcepacks from downloader

--- download-windows-client.ps1
+++ download-windows-client.ps1
@@ -4,12 +4,6 @@ if (Test-Path -Path .\mods) {
 }
 New-Item -Path .\mods -ItemType Directory
 
-Write-Output "Clearing existing resource pack folder."
-if (Test-Path -Path .\resourcepacks) {
-    Remove-Item .\resourcepacks -Force -Recurse -Confirm:$false
-}
-New-Item -Path .\resourcepacks -ItemType Directory
-
 Write-Output "Downloading common mods..."
 foreach($url in Get-Content .\common-mods.txt) {
     Write-Output "Downloading $url"
@@ -25,11 +19,4 @@ foreach($url in Get-Content .\client-mods.txt) {
     Invoke-WebRequest -Uri $url -OutFile $path
 }
 Write-Output ""
-
-Write-Output "Downloading resource packs..."
-foreach($url in Get-Content .\client-resourcepacks.txt) {
-    Write-Output "Downloading $url"
-    $path = ".\resourcepacks\" + $(Split-Path -Path $url -Leaf)
-    Invoke-WebRequest -Uri $url -OutFile $path
-}
 
--- download.sh
+++ download.sh
@@ -35,27 +35,6 @@ function download_mods () {
     destroy_scroll_area
 }
 
-# Download all resource packs within a pack list.
-function download_resources () {
-    source ../progress_bar.sh
-    enable_trapping
-    setup_scroll_area
-
-    file_count=$(sed -n -e '$=' $1)
-    files_done=0
-
-    echo "Downloading $file_count resource packs from $1..."
-    while IFS= read -r url; do
-        echo "Downloading $url."
-        curl -O $url 2>/dev/null
-        files_done=$(( files_done + 1 ))
-        progress=$(echo "scale=2; $files_done/$file_count*100" | bc -l)
-        draw_progress_bar $(echo "$progress/1" | bc)
-    done < $1
-
-    destroy_scroll_area
-}
-
 # Clear mod folder.
 echo "Clearing mod folder, (if exists)"
 rm -rf mods 2>/dev/null
@@ -68,14 +47,6 @@ download_mods "../common-mods.txt"
 # Download client mods only in client mode.
 if [[ $1 == client ]]; then
     download_mods "../client-mods.txt"
-    
-    # Clear resource pack folder.
-    cd ..
-    rm -rf resourcepacks 2>/dev/null
-    mkdir resourcepacks
-    cd resourcepacks
-
-    download_resources "../client-resourcepacks.txt"
 fi
 
 exit 0
